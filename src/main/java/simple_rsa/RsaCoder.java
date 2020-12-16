1149f8f216ae3833dc4a18e5208ef48f335ac116
Adrian Szymanski (ext.) <Adrian.Szymanski@partner.bmw.de>
2020-12-16 11:09:09 +0100

    fix coding/encoding

--- src/main/java/simple_rsa/RsaCoder.java
+++ src/main/java/simple_rsa/RsaCoder.java
@@ -1,16 +1,12 @@
 package simple_rsa;
 
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
-/**
- * TODO ASZ: comment me!
- */
 public class RsaCoder {
     public static int code(final int c, final PublicKey publicKey) {
-        return (int) exponentiate(c, publicKey.getE(), publicKey.getN());
+        return exponentiate(c, publicKey.getE(), publicKey.getN());
     }
 
     public static List<Integer> code(final List<Integer> message, final PublicKey publicKey) {
@@ -19,7 +15,7 @@ public class RsaCoder {
     }
 
     public static int decode(final int c, final PrivateKey privateKey) {
-        return (int) exponentiate(c, privateKey.getD(), privateKey.getN());
+        return exponentiate(c, privateKey.getD(), privateKey.getN());
     }
 
     public static List<Integer> decode(final List<Integer> message, final PrivateKey privateKey) {
@@ -28,8 +24,9 @@ public class RsaCoder {
     }
 
     static int exponentiate(final int long_char, final long exponent, final long modulo) {
-        long result = long_char;
-        for (int i = 1; i < exponent; i++) {
+        long result = 1;
+        // this isn't 100% efficient. One can group exponents like 7 = 4 + 2 + 1
+        for (int i = 0; i < exponent; i++) {
             result = (result * long_char) % modulo;
         }
         return (int) result;
